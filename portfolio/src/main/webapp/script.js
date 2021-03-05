64833991ad4478972ae353ecbb5f7c21876588da
Joshua Kiprono <joshuakiprono8@gmail.com>
2021-03-05 07:16:23 +0000

    RandomMessages

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,11 +1,16 @@
 //Fuction marked with async to show its an asynchronous function
 //await indicates that the function will get a esponse from the server in the background
 /** Fetches the current date from the server and adds it to the page. */
-async function showServerTime() {
-  const responseFromServer = await fetch('/date');
-  const textFromResponse = await responseFromServer.text();
-  const dateContainer = document.getElementById('welcomeSection');
-  dateContainer.innerText =  textFromResponse;
+
+async function getRandomInfo() {
+  const responseFromServer = await fetch('/JoshuaInfo');
+  const Response = await responseFromServer.json();
+  const randomFacts = [Response.one, Response.two, Response.three];
+  // Pick a random fact
+  const fact = randomFacts[Math.floor(Math.random() * randomFacts.length)]
+  // Add it to the page.
+  const factContainer = document.getElementById('fact');
+  factContainer.innerText = '"'+fact+'"';
 }
 
 
