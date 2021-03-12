acc20db5c886cb83b28724d0a8e08ac6c7d75f1e
Joshua Kiprono <joshuakiprono8@gmail.com>
2021-03-12 01:55:18 +0000

    Sentiment Servlet and addition to html

--- portfolio/src/main/java/com/google/sps/servlets/Date.java
+++ portfolio/src/main/java/com/google/sps/servlets/Date.java
@@ -1,7 +1,7 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
-import java.util.Date;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
